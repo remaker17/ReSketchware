813dd306f0c34aa6fd3b1a6ddd1d3d97c8f224a6
TheRemakerMan <farzasz7@gmail.com>
2023-07-06 11:52:00 +0700

    feat: add uncaught exception handler to application class

--- app/src/main/java/app/resketchware/App.java
+++ app/src/main/java/app/resketchware/App.java
@@ -1,17 +1,45 @@
 package app.resketchware;
 
+import android.app.AlarmManager;
 import android.app.Application;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Process;
+import android.util.Log;
 
 import app.resketchware.ui.activities.CrashLogActivity;
 
 public class App extends Application {
 
-    public static Context applicationContext;
+    private static Context applicationContext;
+
+    public static Context getContext() {
+        return applicationContext;
+    }
 
     @Override
     public void onCreate() {
-        super.onCreate();
         applicationContext = getApplicationContext();
+        setupUncaughtExceptionHandler();
+        super.onCreate();
+    }
+
+    private void setupUncaughtExceptionHandler() {
+        Thread.UncaughtExceptionHandler oldHandler = Thread.getDefaultUncaughtExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+            Intent intent = new Intent(applicationContext, CrashLogActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            intent.putExtra(CrashLogActivity.ERROR_EXTRA, Log.getStackTraceString(throwable));
+            PendingIntent pendingIntent = PendingIntent.getActivity(applicationContext, 11111, intent,
+                    PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
+            ((AlarmManager) getSystemService(Context.ALARM_SERVICE))
+                    .set(AlarmManager.ELAPSED_REALTIME_WAKEUP, 1000, pendingIntent);
+            Process.killProcess(Process.myPid());
+            System.exit(1);
+            if (oldHandler != null) {
+                oldHandler.uncaughtException(thread, throwable);
+            }
+        });
     }
 }

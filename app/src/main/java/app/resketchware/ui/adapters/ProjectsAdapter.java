1e22b071078b44e91eed6ec5efc7b64384cff1a5
TheRemakerMan <farzasz7@gmail.com>
2023-07-09 23:44:58 +0700

    revert: "feat: use recycler view's item animator for animating items"

--- app/src/main/java/app/resketchware/ui/adapters/ProjectsAdapter.java
+++ app/src/main/java/app/resketchware/ui/adapters/ProjectsAdapter.java
@@ -1,14 +1,15 @@
 package app.resketchware.ui.adapters;
 
+import android.animation.ValueAnimator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator;
+import androidx.recyclerview.widget.RecyclerView.Adapter;
 
 import app.resketchware.R;
-import app.resketchware.ui.animators.ProjectItemAnimator;
 import app.resketchware.ui.viewholders.ProjectsViewHolder;
 
 import java.util.HashMap;
@@ -17,7 +18,7 @@ import java.util.List;
 /**
  * TODO: Change project model [HashMap<...>] to Project.java
  */
-public class ProjectsAdapter extends RecyclerView.Adapter<ProjectsViewHolder> {
+public class ProjectsAdapter extends Adapter<ProjectsViewHolder> {
 
     private List<HashMap<String, Object>> projects;
     private ProjectSelectionCallback listener;
@@ -43,6 +44,8 @@ public class ProjectsAdapter extends RecyclerView.Adapter<ProjectsViewHolder> {
         HashMap<String, Object> project = projects.get(position);
         holder.bind(project);
         holder.itemView.setOnClickListener(v -> listener.projectClicked(project));
+
+        animateView(holder.itemView);
     }
 
     @Override
@@ -50,14 +53,24 @@ public class ProjectsAdapter extends RecyclerView.Adapter<ProjectsViewHolder> {
         return projects == null ? 0 : projects.size();
     }
 
-    @Override
-    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
-        super.onAttachedToRecyclerView(recyclerView);
-        recyclerView.setItemAnimator(new ProjectItemAnimator());
-    }
-
     public void changeProjectsDataset(List<HashMap<String, Object>> newProjects) {
         this.projects = newProjects;
         notifyDataSetChanged();
+    }
+
+    private void animateView(View itemView) {
+        float startScale = 1.15f;
+        float endScale = 1f;
+        int animationDuration = 300;
+
+        ValueAnimator animator = ValueAnimator.ofFloat(startScale, endScale);
+        animator.setDuration(animationDuration);
+        animator.setInterpolator(new FastOutSlowInInterpolator());
+        animator.addUpdateListener(animation -> {
+            float value = (float) animation.getAnimatedValue();
+            itemView.setScaleY(value);
+            itemView.setScaleX(value);
+        });
+        animator.start();
     }
 }
